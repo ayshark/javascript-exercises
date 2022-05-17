ff736c2953bff432e1c617b5f5dc95c5ccab5357
ayshark <sjaysha@gmail.com>
2022-05-16 21:25:50 -0700

    tested

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  let result = (f - 32) * (5/9);
+  if (result % 1 != 0) return Number(result.toFixed(1));
+  return result;
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  let result = (c * 9/5) + 32;
+  if (result % 1 != 0) return Number(result.toFixed(1));
+  return result;
 };
 
 // Do not edit below this line

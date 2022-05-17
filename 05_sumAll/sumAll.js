b5cf5d70439777168ea1d88ae95e065f607998d0
ayshark <sjaysha@gmail.com>
2022-05-16 21:00:18 -0700

    tested

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (typeof a != 'number' || typeof b != 'number' || a < 0 || b < 0) return "ERROR";
+    let sum = 0;
+    let upperLimit = a > b? a : b;
+    let lowerLimit = upperLimit == a ? b : a;
+    for (let i = lowerLimit; i <= upperLimit; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line

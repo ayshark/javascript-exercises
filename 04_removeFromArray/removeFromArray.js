76cfab3fc263584a063e02aa37e7c53d5664c267
ayshark <sjaysha@gmail.com>
2022-05-16 20:46:41 -0700

    tested

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    let arg = [...args];
+    let result = arr.filter(x => !arg.includes(x));
+    return result;
 };
 
 // Do not edit below this line
